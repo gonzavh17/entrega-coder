19664f5244f9a9863e8212a66f397ce4ee0c136c
gonzavh17 <gonzaasis17@gmail.com>
2023-07-30 18:29:38 -0300

    reentrega 3

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 class Product {
-  constructor(title, description, price, thumbnail, code, stock, id) {
+  constructor(title, description, price, thumbnail, code, stock,) {
     this.title = title;
     this.description = description;
     this.price = price;
@@ -20,17 +20,15 @@ class ProductManager {
   }
 
   getProductById(id) {
-    const searchedProduct = this.products.find(prod => prod.id === id);
+    const searchedProduct = this.products.find((prod) => prod.id === id);
 
-    if(!searchedProduct){
-      console.error("Producto no encontrado")
-    } else return searchedProduct
+    if (!searchedProduct) {
+      console.error("Producto no encontrado");
+    } else return searchedProduct;
   }
 
   addProduct(product) {
     const codeInUse = this.products.find((prod) => prod.code === product.code);
-
-    if (codeInUse) return "Producto existente";
 
     if (
       !product.title ||
@@ -38,28 +36,48 @@ class ProductManager {
       !product.price ||
       !product.thumbnail ||
       !product.code ||
-      !product.stock
+      product.stock < 0
     ) {
-      product.id = this.productIdCounter++;
-      return this.products.push(product);
-    } else {
-      console.log("Todos los campos son obligatorios");
+      console.log("Faltan campos obligatorios");
+      return;
     }
+
+    if (codeInUse) {
+      console.log("Producto existente");
+      return;
+    }
+
+    product.id = this.productIdCounter++;
+    this.products.push(product);
   }
 }
 
-const product1 = new Product("prod1", "este es el prod1", 200, "", "222", 16);
-const product2 = new Product("prod2", "este es el prod2", 300, "", "333", 30);
+const product1 = new Product(
+  "prod1",
+  "este es el prod1",
+  200,
+  "img.jpg",
+  "222",
+  16
+);
+const product2 = new Product(
+  "prod2",
+  "este es el prod2",
+  300,
+  "img.jpg",
+  "333",
+  30
+);
 
 const productManager = new ProductManager();
 
 productManager.addProduct(product1);
 productManager.addProduct(product2);
 
-
-console.log(productManager.getProducts()); /* devuelve los dos products, product1 y product2 */
+console.log(
+  productManager.getProducts()
+); /* devuelve los dos products, product1 y product2 */
 
 console.log(productManager.getProductById(1))
 console.log(productManager.getProductById(2))
 console.log(productManager.getProductById(3)) /* devuelve producto no encontrado */
-
