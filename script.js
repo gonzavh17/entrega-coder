2cc85ae690dac408857cba8d529f2ff737e6af74
gonzavh17 <gonzaasis17@gmail.com>
2023-07-30 02:37:22 -0300

    reentrega

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 class Product {
   constructor(title, description, price, thumbnail, code, stock, id) {
-    this.id
     this.title = title;
     this.description = description;
     this.price = price;
@@ -10,50 +9,57 @@ class Product {
   }
 }
 
-class ProductManager{
-  constructor(){
-    this.products = []
-    this.productIdCounter = 1
+class ProductManager {
+  constructor() {
+    this.products = [];
+    this.productIdCounter = 1;
   }
 
   getProducts() {
-    return this.products
+    return this.products;
   }
 
   getProductById(id) {
-    const searchedProduct = this.products.find(prod => prod.id === id)
+    const searchedProduct = this.products.find(prod => prod.id === id);
 
-    if(searchedProduct){
-      return searchedProduct
-    } else return "Product no encontrado"
+    if(!searchedProduct){
+      console.error("Producto no encontrado")
+    } else return searchedProduct
   }
 
-  addProduct(product){
-    const codeInUse = this.products.find(prod => prod.code === product.code)
+  addProduct(product) {
+    const codeInUse = this.products.find((prod) => prod.code === product.code);
 
-    if(codeInUse) return "Producto existente"
+    if (codeInUse) return "Producto existente";
 
-    if (!product.title || !product.description || !product.price || !product.thumbnail || !product.code || !product.stock){
-      this.products.push(product)
+    if (
+      !product.title ||
+      !product.description ||
+      !product.price ||
+      !product.thumbnail ||
+      !product.code ||
+      !product.stock
+    ) {
+      product.id = this.productIdCounter++;
+      return this.products.push(product);
     } else {
-      return "Todos los campos son obligatorios"
+      console.log("Todos los campos son obligatorios");
     }
-
-    product.id = this.productIdCounter++;
-    this.products.push(product);
-   
   }
 }
 
+const product1 = new Product("prod1", "este es el prod1", 200, "", "222", 16);
+const product2 = new Product("prod2", "este es el prod2", 300, "", "333", 30);
 
-const product1 = new Product("prod1", "este es el prod1", 200, "", "222", 16)
-const product2 = new Product("prod2", "este es el prod2", 300, "", "333", 30)
+const productManager = new ProductManager();
 
-const productManager = new ProductManager()
+productManager.addProduct(product1);
+productManager.addProduct(product2);
 
-productManager.addProduct(product1)
-productManager.addProduct(product2)
+
+console.log(productManager.getProducts()); /* devuelve los dos products, product1 y product2 */
 
 console.log(productManager.getProductById(1))
 console.log(productManager.getProductById(2))
 console.log(productManager.getProductById(3)) /* devuelve producto no encontrado */
+
